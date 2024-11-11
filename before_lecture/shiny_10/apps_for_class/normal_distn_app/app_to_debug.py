7a20a8571877ee628dec2eafdd4dd2c28718aba5
ldsenires <ldsenires@uchicago.edu>
2024-11-11 12:22:24 -0600

    class exercise

--- before_lecture/shiny_10/apps_for_class/normal_distn_app/app_to_debug.py
+++ before_lecture/shiny_10/apps_for_class/normal_distn_app/app_to_debug.py
@@ -1,9 +1,12 @@
 from shiny import App, render, ui
 from shinywidgets import render_altair, output_widget
+import numpy as np
+import pandas as pd
+import altair as alt
 
 app_ui = ui.page_fluid(
     ui.panel_title("Histogram of 200 Draws from Normal with mean mu"),
-    ui.input_slider("mu", "mean mu", 0, 100, 20)
+    ui.input_slider("mu", "mean mu", 0, 100, 20),
     output_widget("my_hist")
 )
 
@@ -11,7 +14,6 @@ def server(input, output, session):
     @render_altair
     def my_hist():
         sample = np.random.normal(input.mu(), 20, 100)
-        print(sample)
         df = pd.DataFrame({'sample': sample})
         return(
             alt.Chart(df).mark_bar().encode(
